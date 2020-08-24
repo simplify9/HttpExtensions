92ce2726942b49c1ef4f20c622748157ef06af16
Samer Awajan <samer@simplify9.com>
2020-08-24 11:42:32 +0300

    querystring to/from object

---
+++ SW.HttpExtensions/IQueryCollectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using SW.PrimitiveTypes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SW.HttpExtensions
+{
+    public static class IQueryCollectionExtensions
+    {
+
+        public static T GetInstance<T>(this IQueryCollection queryCollection)
+        {
+            return (T)queryCollection.GetInstance(typeof(T)); 
+        }
+
+        public static object GetInstance(this IQueryCollection queryCollection, Type type)
+        {
+            try
+            {
+                var obj = Activator.CreateInstance(type);
+                var properties = type.GetProperties();
+                foreach (var property in properties)
+                {
+                    object value = null;
+                    Type propType = property.PropertyType;
+                    if (propType.IsInterface)
+                        throw new SWException($"Type of {property.Name} is an interface. If it's a collection type, consider using List<> or an array.");
+
+                    var queries = queryCollection[property.Name];
+
+                    bool isEnumerable = property.PropertyType.GetInterface(nameof(IEnumerable)) != null;
+
+                    if (isEnumerable)
+                    {
+                        Type nested = propType.GetElementType() ?? propType.GetGenericArguments()[0];
+                        Array tmp = Array.CreateInstance(nested, queries.Count);
+
+                        var queryObjects = queries.Select(q => q.ConvertValueToType(nested));
+                        for (int i = 0; i < queries.Count; i++)
+                            tmp.SetValue(queryObjects.ElementAt(i), i);
+
+                        Type listType = propType.IsInterface ? typeof(List<object>) : propType;
+                        bool isArray = propType.IsArray;
+                        value = isArray ? tmp : Activator.CreateInstance(listType, new object[] { new object[] { tmp } });
+                    }
+                    else
+                    {
+                        value = queries.FirstOrDefault()
+                                .ConvertValueToType(propType);
+                    }
+
+                    if (value == null)
+                        continue;
+
+                    property.SetValue(obj, value, null);
+                }
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                throw new SWException($"Error constructing type: '{type.Name}' from parameters. {ex.Message}");
+
+            }
+        }
+    }
+}
---
+++ SW.HttpExtensions/ObjectExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using SW.PrimitiveTypes;
+
+namespace SW.HttpExtensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToQueryString<T>(this T obj) where T : class
+        {
+            return obj.ToQueryString(typeof(T));
+        }
+
+        public static string ToQueryString(this object obj, Type type)
+        {
+            string[] props = type.GetProperties()
+                .Select(property =>
+                {
+
+                    bool isEnumerable = property.PropertyType.GetInterface(nameof(IEnumerable)) != null;
+                    if (isEnumerable)
+                    {
+                        IEnumerable tmp = ((IEnumerable)property.GetValue(obj).ConvertValueToType(property.PropertyType));
+                        IEnumerable<string> enumerable = tmp.Cast<string>();
+
+                        Type nested = property.PropertyType.GetElementType() ?? property.PropertyType.GetGenericArguments()[0];
+
+                        int length = 0;
+                        foreach (var val in tmp) ++length;
+
+                        Array array = Array.CreateInstance(nested, length);
+
+                        int counter = 0;
+                        foreach (var val in tmp)
+                        {
+                            array.SetValue(val, counter);
+                            ++counter;
+                        }
+
+                        string q = string.Empty;
+                        for (int i = 0; i < array.Length - 1; i++) q += $"{property.Name}={array.GetValue(i)}&";
+                        q += $"{property.Name}={array.GetValue(array.Length - 1)}";
+
+                        return q;
+                    }
+                    else return $"{property.Name}={property.GetValue(obj)}";
+                })
+                .ToArray();
+
+            string queryString = string.Empty;
+            for (int i = 0; i < props.Count() - 1; i++) queryString += props[i] + '&';
+            queryString += props[props.Length - 1];
+
+            return queryString;
+        }
+    }
+}

ba20bca77695108c19e68e0db979fd347693df04
Samer Awajan <samer@simplify9.com>
2020-12-17 12:42:52 +0200

    fix build

--- SW.HttpExtensions/IAppBuilderExtensions.cs
+++ SW.HttpExtensions/IAppBuilderExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SW.PrimitiveTypes;
